70be13e1cf3776b7169a1948de25040c32e778ad
edu-6 <edudiazdl326@gmail.com>
2026-02-03 20:14:18 -0600

    Primer commit en esta rama

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author edu
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Aprendiendo a usar git y github!");
+    }
+}
